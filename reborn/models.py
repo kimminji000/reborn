f0edfa276556614ca5bcb949307c8123f62d884d
kimminji000 <80630563+kimminji000@users.noreply.github.com>
2023-07-17 23:58:18 +0900

    partner page

--- reborn/models.py
+++ reborn/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Partner(models.Model):
+    name = models.CharField(max_length=10)
+    explain = models.CharField(max_length=50)
+    field = models.CharField(max_length=10)
+    img = models.ImageField(upload_to='partner/')
+
+    def __str__(self):
+        return self.name
--- reborn/views.py
+++ reborn/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Partner
 
 # Create your views here.
 def main(request):
     return render(request, 'main.html')
+
+def partner(request):
+    partner = Partner.objects.all()
+    return render(request, 'partner.html', {'partners' : partner})
